99f4a8356c6b528cc438bdee1150eea76bfb7551
liuyi65 <yliu511@jh.edu>
2024-08-22 12:36:21 -0700

    1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <h1>hello world</h1>
+    <h1>hello worldw</h1>
   </React.StrictMode>
 );
 
